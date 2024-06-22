a6d8abae81164320491f6765a3e813c1a95ae4a3
qw941 <qw941689757@gmail.com>
2024-06-22 22:30:49 +0900

    1

--- admin.bat
+++ admin.bat
@@ -1,9 +1,12 @@
-@echo off
+@REM @echo off
 
-@REM 管理员用户执行BAT
+@REM @REM 管理员用户执行BAT
 %1 mshta vbscript:CreateObject("Shell.Application").ShellExecute("cmd.exe","/c %~s0 ::","","runas",1)(window.close)&&exit
 
-@REM 管理员用户执行PS
+@REM @REM 管理员用户执行PS
 powershell -Command "Start-Process powershell.exe -Verb RunAs -ArgumentList '-file','regeditadd.ps1'"
 
 pause
+
+
+
